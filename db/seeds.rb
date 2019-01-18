ce0c4c5b7ff4613dbd61a88a26772c80617e5f07
Sarbik Betal <sarbikb55@gmail.com>
2019-01-18 20:19:48 +0530

    Chat interface changed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,9 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-User.create!(name:  "kate",
-			 enroll: "211111111",
+User.create!(name: "kate",
+             enroll: "211111111",
              email: "kate@gmail.com",
-             password:              "thoughtworks",
+             password: "thoughtworks",
              password_confirmation: "thoughtworks",
              admin: true)
